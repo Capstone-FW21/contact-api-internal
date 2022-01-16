4c2febab7a71b023650402e88fe0527acb7c4e76
DylanHolmesDH3 <86389684+DylanHolmesDH3@users.noreply.github.com>
2022-01-15 16:14:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     description="Backend API for Contact Tracing, Capstone FW21",
     include_package_data=True,
     install_requires=["fastapi>=0.70.0", "uvicorn>=0.15.0", "ctdb_utility_lib>=0.1.8"],
-    name="contact-api",
+    name="contact-api-internal",
     packages=find_packages(),
     python_requires=">=3.5.0",
-    version="0.1.1",
+    version="0.1.0",
 )
